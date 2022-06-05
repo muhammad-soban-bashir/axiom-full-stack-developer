139a7cc84819ed2b509721c5a35bd4c613158875
muhammad-soban-bashir <sobankhan2895@gmail.com>
2022-06-05 19:43:41 +0500

    java script
    java script

--- project1/script.js
+++ project1/script.js
@@ -4,6 +4,15 @@ const email = document.querySelector("#email");
 
 const password = document.getElementById("password");
 const password2= document.getElementById("password2");
+ function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success"
+ }
+
+
+
+
+
 function showError(input,message){
     const formControl= input.parentElement;
     formControl.className = "form-control error"
@@ -16,4 +25,35 @@ form.addEventListener("submit", (e)=>{
      showError(username,"username is required");
 
     }
+    else {
+        showSuccess(username);
+    }
+
+
+    if(email.value===""){
+        showError(email,"email is required");
+   
+       }
+       else {
+           showSuccess(email);
+       }
+
+       if(password.value===""){
+        showError(password,"password is required");
+   
+       }
+       else {
+           showSuccess(password);
+       }
+
+       if(password2.value===""){
+        showError(password2," confirm password is required");
+   
+       }
+       else {
+           showSuccess(password2);
+       }
+
 });
+
+
